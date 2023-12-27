67fbda9d4a1b5e9bbb9d85900cb10bf97e658ac9
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2023-12-27 12:48:30 +0530

    Meal summary added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/layout/Header";
+import MealsSummary from "./components/meals/MealsSummary";
 
 function App() {
   return (
     <div>
-     <Header/>
+      <Header />
+      <MealsSummary />
     </div>
   );
 }
